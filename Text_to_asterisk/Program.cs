3571ad463f581e821ed764fcc62a1c8195510663
Aramayis Gyulbekyan <aramayis.gyulbekyan@coafkids.org>
2020-08-22 23:44:31 +0400

    Add project files.

---
+++ Text_to_asterisk/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TextToAsciiArt;
+
+namespace Text_to_asterisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IArtWriter writer = new ArtWriter();
+            var settings = new ArtSetting
+            {
+                ConsoleSpeed = 100,
+                IsBreakSpace = true,
+                Text = "*",
+                BgText = " "
+            };
+            writer.WriteConsole("ARAM", settings);
+
+
+        }
+    }
+}
