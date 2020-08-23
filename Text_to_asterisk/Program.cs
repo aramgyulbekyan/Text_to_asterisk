a35816a7670e93485c1bdbe2118ae14b1dba3ed6
Aramayis Gyulbekyan <aramayis.gyulbekyan@coafkids.org>
2020-08-23 12:09:48 +0400

    added 3-rd output

--- Text_to_asterisk/Program.cs
+++ Text_to_asterisk/Program.cs
@@ -10,20 +10,36 @@ namespace Text_to_asterisk
             IArtWriter writer = new ArtWriter();
             var settings = new ArtSetting
             {
-                ConsoleSpeed = 50, // Console write speed , the lower value gives moree speed
+                ConsoleSpeed = 50, // Console write speed , the lower value gives more speed
                 Text = "*",  // text symbol
                 BgText = " "  // background symbol
                 
             };
-           
+
             //version with user input 
+
+            //Console.WriteLine("Output 2");
+            //Console.WriteLine("_", 15);
 
             /*Console.WriteLine("please input name");
             string name = Console.ReadLine();
             writer.WriteConsole(name, settings);*/
 
             // version for homework
+
             writer.WriteConsole("ARAM", settings);
+
+            // Writeline output
+            Console.WriteLine("Output 3 ");
+            Console.WriteLine("_",15);
+            Console.WriteLine("     ***    ********      ***    **     **");
+            Console.WriteLine("    ** **   **     **    ** **   ***   ***");
+            Console.WriteLine("   **   **  **     **   **   **  **** ****");
+            Console.WriteLine("  **     ** ********   **     ** ** *** **");
+            Console.WriteLine("  ********* **   **    ********* **     **");
+            Console.WriteLine("  **     ** **    **   **     ** **     **");
+            Console.WriteLine("  **     ** **     **  **     ** **     **");
+
         }
     }
 }
